2ed2aecf13520a194b2da996f24fa9e423b0d9a8
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-08-21 21:49:11 +0700

    menampilkan tutorial

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tutorial;
 use Illuminate\Http\Request;
 
 class TutorialController extends Controller
 {
-  public function create(Request $request)
+  public function index()
+  {
+    return Tutorial::all();
+  }
+
+  public function show($id)
+  {
+    $tutorial = Tutorial::find($id);
+
+    if (!$tutorial) {
+      $tutorial = response()->json([
+        'error' => 'id tutorial tidak di temukan'
+      ], 404);
+    }
+    
+    return $tutorial;
+  }
+
+  public function store(Request $request)
   {
     $request->validate([
       'title' => 'required|unique:tutorials',
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 Route::middleware(['api'])->group(function () {
   Route::post('auth/signup', 'AuthController@signup');
   Route::post('auth/signin', 'AuthController@signin');
+  Route::get('tutorial', 'TutorialController@index');
+  Route::get('tutorial/{id}', 'TutorialController@show');
 
-    Route::middleware(['jwt.auth'])->group(function () {
-      Route::get('/profile', 'UserController@show');
-      Route::post('/tutorial', 'TutorialController@create');
-    });
+  Route::middleware(['jwt.auth'])->group(function () {
+    Route::get('/profile', 'UserController@show');
+    Route::post('/tutorial', 'TutorialController@store');
+  });
 });
