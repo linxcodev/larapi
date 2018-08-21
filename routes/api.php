b532a6f3783430f72dc58eb5444a4a89199db064
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-08-21 16:57:17 +0700

    middleware jwt

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 Route::middleware(['api'])->group(function () {
   Route::post('auth/signup', 'AuthController@signup');
   Route::post('auth/signin', 'AuthController@signin');
+
+    Route::middleware(['jwt.auth'])->group(function () {
+      Route::get('profile', 'UserController@show');
+    });
 });
